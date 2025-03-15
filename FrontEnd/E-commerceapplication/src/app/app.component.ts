5fb10efbbef25bb57c4a988687bd464440f19f46
shadia <162451775+shadiaal@users.noreply.github.com>
2025-03-16 02:37:22 +0300

    Update app.component.ts

--- FrontEnd/E-commerceapplication/src/app/app.component.ts
+++ FrontEnd/E-commerceapplication/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-// import {ProductsComponent} from './products/products.component'
 import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
